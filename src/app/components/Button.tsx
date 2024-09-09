2e852fef5fa91576b62036c457f25c3b434d21a1
CoinDegen <polydegen@protonmail.com>
2024-09-09 15:45:56 +0530

    install shadcn

--- src/app/components/Button.tsx
+++
@@ -1,9 +0,0 @@
-<button
-  onClick={handleClick}
-  className={`inline-flex h-28 w-28 items-center justify-center rounded-3xl bg-white p-7 transition-transform duration-300 hover:scale-105 ${
-    isLoading ? "scale-95 opacity-75" : ""
-  }`}
-  disabled={isLoading}
->
-  <DreamLogo />
-</button>;
